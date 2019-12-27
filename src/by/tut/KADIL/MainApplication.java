721bad59a2032f184722570d379f4deda32fc5e3
NotNilfgaardian <danikkudryshov@gmail.com>
2019-12-27 20:20:41 +0300

    добавляем главный файл

---
+++ src/by/tut/KADIL/MainApplication.java
@@ -0,0 +1,13 @@
+package by.tut.KADIL;
+
+import javax.swing.*;
+
+public class MainApplication
+{
+    public static void main(String[] args)
+    {
+        SimpleGUI app = new SimpleGUI();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+}
